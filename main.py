d7adc13056b8456777387636a3308a04bbec7adf
Stefan Heinecke <stefan.heinecke1@gmail.com>
2026-02-27 10:21:57 +0100

    ewfewfsfd

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# CORS erlauben, damit dein HTML/JS im Browser zugreifen darf
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # für Entwicklung ok
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/text")
 def get_text():
